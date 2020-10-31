fb299f5ad88c027bf6a0f833aa630eb994f24d5e
Richard <Richard>
2020-10-30 20:34:40 -0700

    get innerHTML code

--- wasm-script.js
+++ wasm-script.js
@@ -1,16 +1,65 @@
 class WasmScript extends HTMLElement {
     constructor() {
-      // Always call super first in constructor
-      super();
+        super();
     }
 
-    async compile(){
-        return {
-            add(a,b){
-                return a+b;
-            }
+    connectedCallback() {
+        if(this.innerHTML === undefined){
+            this.setup()
+        } else {
+            this.code = this.innerHTML;
+            this.innerHTML = "";
         }
     }
-  }
 
-  customElements.define('wasm-script', WasmScript);
+    childrenAvailableCallback() {
+        this.code = this.innerHTML;
+        this.innerHTML = "";
+        this.codeResolver(this.code)
+    }
+
+    // https://stackoverflow.com/questions/48498581/textcontent-empty-in-connectedcallback-of-a-custom-htmlelement
+    setup() {
+        self.parentNodes = []
+        let el = this;
+        while (el.parentNode) {
+            el = el.parentNode
+            this.parentNodes.push(el)
+        }
+        if ([this, ...this.parentNodes].some(el => el.nextSibling) || document.readyState !== 'loading') {
+            this.childrenAvailableCallback();
+        } else {
+            this.mutationObserver = new MutationObserver(() => {
+                if ([this, ...this.parentNodes].some(el => el.nextSibling) || document.readyState !== 'loading') {
+                    this.childrenAvailableCallback()
+                    this.mutationObserver.disconnect()
+                }
+            });
+            this.mutationObserver.observe(this, { childList: true });
+        }
+    }
+
+    async compile() {
+        if(this.innerHTML){
+            this.code = this.innerHTML;
+            this.innerHTML = "";
+        }
+        if(this.code){
+            return await this.compileCode(this.code)
+        }
+        await new Promise((resolve)=>{
+            this.codeResolver = resolve;
+        });
+        return await this.compileCode(this.code)
+    }
+
+    async compileCode(code){
+        return {
+            add(a, b) {
+                return a + b;
+            }
+        };
+    }
+}
+
+customElements.define('wasm-script', WasmScript);
