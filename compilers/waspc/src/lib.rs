cb9b9a03f7377a72b2836ce6cbe5040f8e48214e
Richard <Richard>
2020-11-22 09:42:42 -0800

    new api

--- compilers/waspc/src/lib.rs
+++ compilers/waspc/src/lib.rs
@@ -1,4 +1,4 @@
-use wasm_compiler::{process,log};
+use wasm_compiler::{log, process};
 use wasp_core::{compiler, parser};
 
 #[no_mangle]
@@ -6,7 +6,13 @@ pub fn compile(code_ptr: usize) -> usize {
     process(code_ptr, |code| {
         log("compiling the code below!!");
         log(&code);
-        let app = parser::parse(code)?;
-        Ok(compiler::compile(app)?)
+        let app = match parser::parse(code) {
+            Ok(a) => a,
+            Err(e) => return Err(e.to_string()),
+        };
+        Ok(match compiler::compile(app) {
+            Ok(a) => a,
+            Err(e) => return Err(e.to_string()),
+        })
     })
 }
